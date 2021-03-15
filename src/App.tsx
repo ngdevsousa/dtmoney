ca16c9a437a67c68b7759f57a02e25fd5a296629
f.sousa <ng.dev.sousa@outlook.com>
2021-03-15 19:45:43 -0300

    Add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+        <GlobalStyle>
+          
+        </GlobalStyle>
     </div>
   );
 }
